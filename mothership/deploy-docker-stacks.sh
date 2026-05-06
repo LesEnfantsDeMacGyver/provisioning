91c2112ded3af5d7df70e3894cd97b7c77314055
Ian Bernardo <ian.bernardo@frizull.net>
2026-05-06 03:37:49 +0200

    Create docker_stacks external volumes

--- mothership/deploy-docker-stacks.sh
+++ mothership/deploy-docker-stacks.sh
@@ -79,6 +79,7 @@ ssh_target REMOTE_DIR="${REMOTE_DIR}" 'bash -s' <<'REMOTE'
 set -Eeuo pipefail
 
 cd "${REMOTE_DIR}"
+sudo docker volume create docker_stacks_chataigne >/dev/null
 sudo docker compose up -d --build
 sudo docker compose ps
 REMOTE
